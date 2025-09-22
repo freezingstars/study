cf6f2202d5ceb6f7bf50ca00697654be585a77cc
freezingstars <603932130@qq.com>
2025-09-22 23:42:27 +0800

    end9.22

--- PHP/case/test4trans.php
+++ PHP/case/test4trans.php
@@ -1,5 +1,14 @@
 <?php
     $a = 'abc1.1.1';
     $b = '1.1.1abc';
-    echo (float)$a + (float)$b;
+    echo (float)$a + (float)$b.'<br>';
+    $num = 24;
+    $bin_num = Decbin($num);
+    $oct_num = Decoct($num);
+    $hex_num = DecHex($num);
+    $bin_hex = bin2hex('10');
+    echo $bin_num.'<br>';
+    echo $oct_num.'<br>';
+    echo $hex_num.'<br>';
+    echo $bin_hex.'<br>';
     ?>
